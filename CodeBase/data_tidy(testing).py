a10d3dc645d61c1e9e24438ce910616fc6903d48
Jiadi YU <yujd10@outlook.com>
2023-09-30 10:38:57 -0400

    Update data_tidy(testing).py

--- CodeBase/data_tidy(testing).py
+++ CodeBase/data_tidy(testing).py
@@ -5,7 +5,7 @@ import numpy as np
 
 #for each game(only draft and test rn)
 def json_reader(json_path:str) -> pd.DataFrame:
-    with open('./nhl_data/2016020001.json') as f:
+    with open(json_path) as f:
         game_json = json.load(f)
         # game time/period information,game ID,team information (which team took the shot),shot/goal,coordinates,
         # the shooter name,the goalie name, shot type(if it was on an empty net), and whether or not a goal was at even strength, shorthanded, or on the power play.
@@ -26,7 +26,7 @@ def json_reader(json_path:str) -> pd.DataFrame:
                 shot_idxs.append(play["about"]["eventIdx"])
 
         rows = []
-        # gathering shots' and goals' informations(I'm trying to use dataframe directly here)
+        # gathering shots' and goals' informations
         # shot
         for shot_idx in shot_idxs:
             item_row = []
