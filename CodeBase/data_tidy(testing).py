237463043c437677bb3902e2b7a5ceff1772ae3b
Jiadi YU <yujd10@outlook.com>
2023-09-30 00:52:38 -0400

    extracting informations

--- CodeBase/data_tidy(testing).py
+++ CodeBase/data_tidy(testing).py
@@ -6,15 +6,16 @@ import pandas
 with open('./nhl_data/2016020001.json') as f:
     game_json = json.load(f)
     # game time/period information,game ID,team information (which team took the shot),shot/goal,coordinates,
-    # the shooter name,the goalie name, shot type(if it was on an empty net, and whether or not a goal was at even strength, shorthanded, or on the power play).
+    # the shooter name,the goalie name, shot type(if it was on an empty net), and whether or not a goal was at even strength, shorthanded, or on the power play.
     game_time = game_json["gameData"]["datetime"]
     game_id = game_json["gamePk"]
 
-    plays = game_json["liveData"]["plays"]
+    plays = game_json["liveData"]["plays"]["allPlays"]
 
     # gathering shots' and goals' indexes
     goal_idx = game_json["liveData"]["plays"]["scoringPlays"]
     shot_idxs = []
+
     for play in plays:
         if play["result"]["event"] == "Shot":
             shot_idxs.append(play["about"]["eventIdx"])
@@ -22,7 +23,29 @@ with open('./nhl_data/2016020001.json') as f:
     # gathering shots' and goals' informations(I'm trying to use dataframe directly here)
     # shot
     for shot_idx in shot_idxs:
-        pass
+        shot = plays[shot_idx]
+        attack_team = shot["team"]
+        attack_team_id = shot["team"]["id"]
+        attack_team_name = shot["team"]["name"]
+        attack_team_triCode = shot["team"]["triCode"]
+        play_type = "Shot"
+        coordinate= (shot["coordinates"]["x"],shot["coordinates"]["y"])
+        players = shot["players"]
+        for player in players:
+            if player["playerType"]== "Goalie":
+                goalie = player
+                goalie_name = player["fullName"]
+            elif player["playerType"]== "Shooter":
+                shooter = player
+                shooter_name = player["fullName"]
+       
+               # ...
+        empty_Net = None
+        strength = None
+    
+    #goal
+    
+
     
     #TODO
     #TODO
