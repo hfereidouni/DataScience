577df6738927344a79692c1e9d9aa90f12419b91
Jiadi YU <yujd10@outlook.com>
2023-09-30 00:28:46 -0400

    little progress in tidy part

--- CodeBase/test.py
+++ CodeBase/test.py
@@ -3,5 +3,33 @@ import os
 import pandas
 
 with open('./nhl_data/2016020001.json') as f:
-    q=json.load(f)
-    print(q)
+    game_json = json.load(f)
+    # game time/period information,game ID,team information (which team took the shot),shot/goal,coordinates,
+    # the shooter name,the goalie name, shot type(if it was on an empty net, and whether or not a goal was at even strength, shorthanded, or on the power play).
+    game_time = game_json["gameData"]["datetime"]
+    game_id = game_json["gamePk"]
+
+    plays = game_json["liveData"]["plays"]
+
+    # gathering shots' and goals' indexes
+    goal_idx = game_json["liveData"]["plays"]["scoringPlays"]
+    shot_idxs = []
+    for play in plays:
+        if play["result"]["event"] == "Shot":
+            shot_idxs.append(play["about"]["eventIdx"])
+
+    # gathering shots' and goals' informations(I'm trying to use dataframe directly here)
+    # shot
+    for shot_idx in shot_idxs:
+        pass
+    
+    #TODO
+    #TODO
+    #TODO
+    #TODO
+    #TODO
+    #TODO#TODO
+    #TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO#TODO
+       
+
+    
