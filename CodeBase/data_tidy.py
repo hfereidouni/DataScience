ddfd9f0676bffab019ab5f9bc6f01d0cf73dbcc3
Jiadi YU <yujd10@outlook.com>
2023-10-10 21:39:25 -0400

    comments and texts

--- CodeBase/data_tidy.py
+++ CodeBase/data_tidy.py
@@ -5,14 +5,30 @@ import numpy as np
 from tqdm import tqdm
 import re
 
-#for each game(only draft and test rn)
-
 #Rink_X = [-100,100]
 #Rink_Y = [-42.5,42.5]
 #Distance between nets and edges are 11 ft, 100 - 11 = 89
 LEFT_NET_COOR = [-89,0]
 RIGHT_NET_COOR = [89,0]
 def json_reader(json_path:str) -> pd.DataFrame:
+    """This function will take a path of a json filse as input and tidy data inside,
+    it will return tidied data in a pandas dataframe.
+    The features we have included here are:
+        {"game_id", "event_idx","play_type","shot_type","shot_dist", "game_time", 
+    "goals_home", "goals_away",  "attack_team_name", "period", "period_time_rem", 
+    "coordinate", "shooter_name", "goalie_name", "empty_Net","strength"} which most 
+    of them can be extracted directly from json file, only "shot_dist" which represents
+    the distance from the shot coordinate to the net coordinate needs to be calculated
+    with help of rink_side attributes and team information. For those empty information
+    or attributes, we replace them with None.
+    
+    Parameters:
+    json_path(str): Path for the json file of a game
+
+    Returns:
+    Pandas Dataframe:Tidied data
+
+   """
     with open(json_path) as f:
         game_json = json.load(f)
         game_time = game_json["gameData"]["datetime"]["dateTime"]
@@ -29,21 +45,19 @@ def json_reader(json_path:str) -> pd.DataFrame:
             item_row = []
             if play["result"]["event"] == "Shot" or play["result"]["event"]=="Goal":
                 play_idx = play["about"]["eventIdx"]
-                # print(play_idx)
                 period = play["about"]["period"]
                 period_type = play["about"]["periodType"]
-                # period_time = play["about"]["periodTime"]
+                period_time_rem = play["about"]["periodTimeRemaining"]
                 goals_home = play["about"]["goals"]["home"]
                 goals_away = play["about"]["goals"]["away"]
-                period_time_rem = play["about"]["periodTimeRemaining"]
                 attack_team = play["team"]
                 attack_team_name = play["team"]["name"]
                 play_type = play["result"]["event"]
                 shot_type = None
+                
                 if "secondaryType" in play["result"]:
                     shot_type = play["result"]["secondaryType"]
                 coordinate= play["coordinates"]
-                # print(coordinate) 
                 players = play["players"]
                 goalie_name = None
                 shooter_name = None
@@ -59,7 +73,8 @@ def json_reader(json_path:str) -> pd.DataFrame:
                         empty_Net = play["result"]["emptyNet"]
                     strength = play["result"]["strength"]["code"]
                 
-                #calculate the distance between play coordinates and net(that should be attacked) coordinate
+                #find the rinkside that each team should attack/defend
+                #and calculate the distance between play coordinates and net(attack rinkside) coordinate
                 shot_dist = None
                 if coordinate != {}:
                     if "x" not in coordinate:
@@ -67,6 +82,7 @@ def json_reader(json_path:str) -> pd.DataFrame:
                     elif "y" not in coordinate:
                         coordinate["y"] = 0
                     
+                    #find rinkside
                     periods = game_json["liveData"]["linescore"]["periods"]
                     rink_side = None
                     if attack_team_name == team_home["name"]:
@@ -82,6 +98,7 @@ def json_reader(json_path:str) -> pd.DataFrame:
                                 if "rinkSide" in p[home_or_away]:
                                     rink_side = p[home_or_away]["rinkSide"]
                     
+                    #calculate distance
                     if rink_side == "left":
                         shot_dist = np.linalg.norm(np.array([coordinate["x"],coordinate["y"]]) - np.array(RIGHT_NET_COOR))
                     elif rink_side == "right":
@@ -89,7 +106,7 @@ def json_reader(json_path:str) -> pd.DataFrame:
                     else:
                         shot_dist = None
                     
-                
+                #list for each event/play
                 item_row = [game_id,
                             play_idx,
                             play_type,
@@ -104,8 +121,11 @@ def json_reader(json_path:str) -> pd.DataFrame:
                             coordinate,shooter_name,
                             goalie_name,
                             empty_Net,strength]
+                
+                #total list for all events/games
                 rows.append(item_row)
-
+        
+        #list->pd.DataFreame
         df = pd.DataFrame(rows,columns=[["game_id",
                                         "event_idx",
                                         "play_type",
@@ -160,6 +180,3 @@ def read_all_game(path:str)->pd.DataFrame:
 
     print("Finieshed.")
     return result
-
-# read_all_game("./nhl_data/").to_csv('./tidy_test_1.csv', sep=',',index=False) 
-# read_a_season("./nhl_data/",2016).to_csv('./tidy_{season}.csv'.format(season=2016), sep=',',index=False)
