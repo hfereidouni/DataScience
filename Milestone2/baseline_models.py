3930820bf51db343d5feceab24520fc105c07873
Jerome Francis <36845045+jerryfrancis-97@users.noreply.github.com>
2023-11-16 12:03:35 -0500

    update: improve viz

--- Milestone2/baseline_models.py
+++ Milestone2/baseline_models.py
@@ -13,6 +13,64 @@ import argparse
 import os
 
 
+def calc_percentile_goal_rate_data_display_random(class_probabilities, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment):
+
+    #code from Task 6
+    probas_and_label = pd.DataFrame(Y_valid.copy())
+    probas_and_label['goal_proba']=class_probabilities[:,1]
+    probas_and_label = probas_and_label.sort_values(by='goal_proba', ascending=False)
+    probas_and_label['#goal+#shot'] = range(1,len(probas_and_label)+1)
+
+    shot_and_goal = np.array(probas_and_label.loc[:,'is_goal'])
+
+    #cumulative sum of goals based on percentile
+    goal = np.zeros(len(shot_and_goal))
+    for i in range(len(shot_and_goal)):
+        goal[i] = shot_and_goal[:i].sum()
+    probas_and_label['#goal'] =  goal
+
+    #adding other params
+    probas_and_label['#goal/#goal+#shot'] = 100*probas_and_label['#goal']/probas_and_label['#goal+#shot']
+    probas_and_label['rank'] = probas_and_label['goal_proba'].rank(method="first", pct=True)*100
+    probas_and_label['goal_prob_sum'] = probas_and_label['#goal/#goal+#shot'].sum()
+    probas_and_label['goal_prob_cumulative_sum'] = probas_and_label['#goal/#goal+#shot'].cumsum()
+    probas_and_label['cum_percent'] = 100*(probas_and_label['goal_prob_cumulative_sum'] / probas_and_label['goal_prob_sum'])
+    
+    plot_x_values = probas_and_label['rank'].iloc[int(len(probas_and_label)*0.01):]
+    plot_y_values = probas_and_label['#goal/#goal+#shot'].iloc[int(len(probas_and_label)*0.01):]
+    
+    goal_rate = plt.plot(plot_x_values,plot_y_values,label=name_placeholder)
+    
+    # plt.plot(plot_x_values,plot_y_values,label=name_placeholder)
+    plt.xlim([105,-5])
+    plt.ylim([-5,105])
+    plt.grid(True)
+    plt.ylabel("Goals/(Shots+Goals) (%)")
+    plt.xlabel("Shot probability model percentile")
+    plt.title(f"Goal Rate for {titlename} with {modelname}")
+    plt.legend()
+    plt.savefig(os.path.join(BASELINE_FOLDER_PATH, f"{modelname}_goalratepercentile_{name_placeholder}.png"))
+    experiment.log_figure(figure_name=f"{modelname}_goalratepercentile_{name_placeholder}")
+    plt.close()
+    print("Saving goal rate vs shot percentile curve!")
+
+    cum_goal_rate = plt.plot(probas_and_label['rank'],probas_and_label['cum_percent'],label=name_placeholder)
+    
+    # plt.plot(probas_and_label['rank'],probas_and_label['cum_percent'],label=name_placeholder)
+    plt.xlim([105,-5])
+    plt.ylim([-5,105])
+    plt.grid(True)
+    plt.ylabel("Proportion (%)")
+    plt.xlabel("Shot probability model percentile")
+    plt.title(f"Cumulative % of goals for {titlename} with {modelname}")
+    plt.legend()
+    plt.savefig(os.path.join(BASELINE_FOLDER_PATH, f"{modelname}_cumulativegoalrate_{name_placeholder}.png"))
+    experiment.log_figure(figure_name=f"{modelname}_cumulativegoalrate_{name_placeholder}")
+    plt.close()
+    print("Saving cumulative goal rate vs shot percentile curve!")
+
+    return goal_rate, cum_goal_rate
+
 
 
 def calc_percentile_goal_rate_data_display(class_probabilities, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment):
@@ -43,7 +101,7 @@ def calc_percentile_goal_rate_data_display(class_probabilities, Y_valid, BASELIN
     
     goal_rate = plt.plot(plot_x_values,plot_y_values,label=name_placeholder)
     
-    plt.plot(plot_x_values,plot_y_values,label=name_placeholder)
+    # plt.plot(plot_x_values,plot_y_values,label=name_placeholder)
     plt.xlim([105,-5])
     plt.ylim([-5,105])
     plt.grid(True)
@@ -58,7 +116,7 @@ def calc_percentile_goal_rate_data_display(class_probabilities, Y_valid, BASELIN
 
     cum_goal_rate = plt.plot(probas_and_label['rank'],probas_and_label['cum_percent'],label=name_placeholder)
     
-    plt.plot(probas_and_label['rank'],probas_and_label['cum_percent'],label=name_placeholder)
+    # plt.plot(probas_and_label['rank'],probas_and_label['cum_percent'],label=name_placeholder)
     plt.xlim([105,-5])
     plt.ylim([-5,105])
     plt.grid(True)
@@ -73,6 +131,23 @@ def calc_percentile_goal_rate_data_display(class_probabilities, Y_valid, BASELIN
 
     return goal_rate, cum_goal_rate
 
+def display_calibration_from_pred(clf,X_valid, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment):
+
+    # disp = CalibrationDisplay.from_predictions(Y_valid.to_numpy(), class_probabilities[:,1])
+    
+    prob_true, prob_pred = calibration_curve(Y_valid, clf.predict(X_valid))
+    y_prob = clf.predict_proba(X_valid)
+    disp = CalibrationDisplay(prob_true, prob_pred, y_prob)
+    disp = disp.plot(label="Random Classifer")
+    plt.title(f"Reliability curve for {titlename} with {modelname}")
+    plt.grid(True)
+    plt.show()
+    plt.savefig(os.path.join(BASELINE_FOLDER_PATH, f"{modelname}_calibration_{name_placeholder}.png"))
+    experiment.log_figure(figure_name=f"{modelname}_calibration_{name_placeholder}")
+    plt.close()
+    print("Saving Calibration curve!!")
+
+    return disp
 
 def display_calibration(clf, X_valid, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment):
 
@@ -102,7 +177,7 @@ def display_roc(class_probabilities, Y_valid, BASELINE_FOLDER_PATH, titlename, m
                                      # Y_pred,
                                     # name=f"{name_placeholder}")
     roc_plot = plt.plot(fpr,tpr,'b',label=f"{name_placeholder}, AUC ={auc_score}")
-    plt.plot(fpr,tpr,'b',label=f"{name_placeholder}, AUC ={auc_score}")
+    # plt.plot(fpr,tpr,'b',label=f"{name_placeholder}, AUC ={auc_score}")
     plt.plot([0, 1], [0, 1],'r--')
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
@@ -237,10 +312,10 @@ def random_uniform_classifier(X_train, X_valid, Y_train, Y_valid, features_list,
     roc_plot = display_roc(class_probabilities, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment)
     
     # Goal rate v/s Shot probability percentile
-    goal_rate_plot, cum_goal_rate_plot = calc_percentile_goal_rate_data_display(class_probabilities, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment)
+    goal_rate_plot, cum_goal_rate_plot = calc_percentile_goal_rate_data_display_random(class_probabilities, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment)
 
     #Calibration Display   
-    cali_plot = display_calibration(dummy_clf, X_valid, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment)
+    cali_plot = display_calibration_from_pred(dummy_clf, X_valid, Y_valid, BASELINE_FOLDER_PATH, titlename, modelname, name_placeholder, experiment)
     print(f"-----Experiment using Random Classifier COMPLETED!-------")
 
     return roc_plot, goal_rate_plot, cum_goal_rate_plot, cali_plot, AUC_score
@@ -268,17 +343,18 @@ def experiment_setup(features_for_model, model_code, name_placeholder, titlename
              }
 
     # Train Classifier
-    roc_plot, goal_rate_plot, cum_goal_rate_plot, cali_plot, auc = models[model_code](X_train,
-                       X_valid,
-                       Y_train,
-                       Y_valid,
-                       name_placeholder=name_placeholder,
-                       titlename=titlename,
-                       features_list=features_for_model,
-                       BASELINE_FOLDER_PATH=BASELINE_FOLDER_PATH,
-                       BASELINE_MODEL_DIR=BASELINE_MODEL_DIR,
-                       experiment=experiment,
-                      )
+    roc_plot,goal_rate_plot,cum_goal_rate_plot,cali_plot, auc = models[model_code](
+                                X_train,
+                                X_valid,
+                                Y_train,
+                                Y_valid,
+                                name_placeholder=name_placeholder,
+                                titlename=titlename,
+                                features_list=features_for_model,
+                                BASELINE_FOLDER_PATH=BASELINE_FOLDER_PATH,
+                                BASELINE_MODEL_DIR=BASELINE_MODEL_DIR,
+                                experiment=experiment,
+                            )
 
     # plotting graphs
     # get x y data from plt objects
