197eb63df0b5cc25aa951df5ef866624e2d9b0ef
Jiadi YU <yujd10@outlook.com>
2023-12-06 21:10:36 -0500

    app.py and flask_app.ipynb for testing

--- Milestone3/serving/app.py
+++ Milestone3/serving/app.py
@@ -17,43 +17,51 @@ import pandas as pd
 import joblib
 from comet_ml import API
 
-
-# import ift6758
-
-
 LOG_FILE = os.environ.get("FLASK_LOG", "flask.log")
 MODEL_DIR = "./models/"
-API_KEY = "v5q8O8LftZtvOcoXlVM8Ku8fH"
-WORK_SPACE = "hfereidouni"
-VALID_MODELS = [
-    "Log_reg_shot_dist_only",
-    "Log_reg_shot_dist_and_angle",
-]
-NEWEST_MODELS_VERSION = {"Log_reg_shot_dist_only":"1.17.0",
-                         "Log_reg_shot_dist_and_angle":"1.9.0"}
-DEFAULT_MODEL = VALID_MODELS[0]
-DEFAULT_MODEL_PATH = MODEL_DIR+DEFAULT_MODEL+'.joblib'
-LOADED_MODEL = None
-
 app = Flask(__name__)
 
-def download_model(api_key,work_space,model_name,version,output_folder):
-    api = API(api_key)
-    model = api.get_model(workspace=work_space, model_name=model_name)
-    model.download(version=version,output_folder=output_folder,expand=True)
+#helper class
+class CometClient(object):
+    def __init__(self, api_key, work_place,valid_models,newest_model_version):
+        self.api_key = api_key
+        self.work_place = work_place
+        self.valid_models = valid_models
+        self.newest_model_version = newest_model_version
+        self.default_model = self.valid_models[0]
+        
+    def model_exist(self,model_name):
+        if(model_name not in list(map(str.lower,self.valid_models))):
+            return False
+        return True
 
+    def download_model(self,model_name,version,output_folder):
+        api = API(self.api_key)
+        model = api.get_model(workspace=self.work_place, model_name=model_name)
+        model.download(version=version,output_folder=output_folder,expand=True)
+
+API_KEY = "v5q8O8LftZtvOcoXlVM8Ku8fH" #os.environ.get("COMET_API_KEY") if get from env var
+WORK_PLACE = "hfereidouni"
+COMET_CLIENT = None # will be initialized at @app.before_first_request
+LOADED_MODEL = None # will be initialized at @app.before_first_request
+
+# helper functions
 def load_model(model_path):
+    global LOADED_MODEL
     model = joblib.load(model_path)
-    return model
+    LOADED_MODEL = model
 
-def model_exist(model_path=DEFAULT_MODEL_PATH):
+def model_file_exist(model_path):
     if os.path.isfile(model_path):
         return True
     return False
 
 @app.route("/check", methods=["GET"])
 def check():
-    return LOADED_MODEL.feature_names_in_
+    if LOADED_MODEL != None:
+        return jsonify(LOADED_MODEL.feature_names_in_.tolist())
+    else:
+        return jsonify("No model is loaded yet"),400
 
 
 @app.before_first_request
@@ -62,37 +70,48 @@ def before_first_request():
     Hook to handle any initialization before the first request (e.g. load model,
     setup logging handler, etc.)
     """
-    # TODO: setup basic logging configuration
+    #setup basic logging configuration
     logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 
-    # TODO: any other initialization before the first request (e.g. load default model)
-    global LOADED_MODEL #This is to change a global variable
+    #initialization: CometClient and Loading default model
+    global COMET_CLIENT
 
-    if model_exist(DEFAULT_MODEL_PATH):
-        LOADED_MODEL = load_model(DEFAULT_MODEL_PATH)
-        app.logger.info(f'Default model loaded, {DEFAULT_MODEL} version {NEWEST_MODELS_VERSION[DEFAULT_MODEL]}')
-    else:
-        app.logger.info(f'Default model not found locally, download started...')
-        download_model(API_KEY,WORK_SPACE,DEFAULT_MODEL,version=NEWEST_MODELS_VERSION[DEFAULT_MODEL],output_folder=MODEL_DIR)
-        app.logger.info(f'Default model downloaded, {DEFAULT_MODEL} version {NEWEST_MODELS_VERSION[DEFAULT_MODEL]}')
-        LOADED_MODEL = load_model(DEFAULT_MODEL_PATH)
-        app.logger.info(f'Default model downloaded downloaded')
+    COMET_CLIENT = CometClient(api_key=API_KEY, work_place = WORK_PLACE,
+                           valid_models=["Log_reg_shot_dist_only",
+                                        "Log_reg_shot_dist_and_angle"],
+                            newest_model_version={"Log_reg_shot_dist_only":"1.17.0",
+                                                     "Log_reg_shot_dist_and_angle":"1.9.0"})
 
+    default_model = COMET_CLIENT.default_model
+    default_path = MODEL_DIR+default_model+'.joblib'
+    versions = COMET_CLIENT.newest_model_version
+    
+    # download default model
+    if not model_file_exist(default_path):
+        try:
+            app.logger.info(f'Default model not found locally, download started...')
+            COMET_CLIENT.download_model(default_model,version=versions[default_model],output_folder=MODEL_DIR)
+            app.logger.info(f'Default model downloaded, {default_model} version {versions[default_model]}')
+        #download failed
+        except Exception:
+            exception = f"Default model failed downloading, the loaded model is None for now"
+            abort(400,exception)
+    load_model(default_path)
+    app.logger.info(f'Default model is loaded as {default_model}')
 
 @app.route("/logs", methods=["GET"])
 def logs():
     """Reads data from the log file and returns them as the response"""
     
-    # # TODO: read the log file specified and return the data
-    # raise NotImplementedError("TODO: implement this endpoint")
-
     try:
-        f = open('flask.log','r')
+        text = Path('flask.log').read_text()
     except OSError:
-        app.logger.exception("Log file not found")
+        exception = "Log read failed"
+        app.logger.exception(exception)
+        return jsonify(exception),400
 
-    response = f.read()
-    app.logger.info("logs read.")
+    response = text.splitlines()
+    app.logger.info("Logs read with success.")
     return jsonify(response)
 
 
@@ -103,46 +122,53 @@ def download_registry_model():
 
     The comet API key should be retrieved from the ${COMET_API_KEY} environment variable.
 
-    Recommend (but not required) json with the schema:
+    input jason looks like:
 
-        {
-            workspace: (required),
-            model: (required),
-            version: (required),
-            ... (other fields if needed) ...
-        }
+        request = {
+            "model_name":"name",
+           "version":"version"
+           }
     
     """
     # Get POST json data
     json = request.get_json()
     app.logger.info(json)
 
+    #read from request
     model_name = json["model_name"]
     version = json["version"]
 
-    global LOADED_MODEL
-
     model_path = MODEL_DIR+model_name+'.joblib'
-    # TODO: check to see if the model you are querying for is already downloaded
-    if os.path.isfile(model_path):
-    # TODO: if yes, load that model and write to the log about the model change.  
-    # eg: app.logger.info(<LOG STRING>)
-        app.logger.info(f"Loaded Model has changed from {LOADED_MODEL} to {model_name}")
-        LOADED_MODEL = load_model(model_path)
+    #check if the model exists in comet library
+    if not COMET_CLIENT.model_exist(model_name):
+        exception = f"{model_name} is not a valide model"
+        return jsonify(exception), 400
     else:
-        response = "No"
-    # TODO: if no, try downloading the model: if it succeeds, load that model and write to the log
-    # about the model change. If it fails, write to the log about the failure and keep the 
-    # currently loaded model
+        #check to see if the model you are querying for is already downloaded
+        if model_file_exist(model_path):
+        #if yes, load that model and write to the log about the model change.  
+            load_model(model_path)
+            response = f"Loaded Model has changed to {model_name}"
+            app.logger.info(response)
+        else:
+        #download model
+            try:
+                COMET_CLIENT.download_model(model_name,version=version,output_folder=MODEL_DIR)
+                #saving successfully
+                if model_file_exist(model_path):
+                    response = f"{model_name} downloaded and loaded as the current model."
+                    app.logger.info(response)
+                    load_model(model_path)
 
-    # Tip: you can implement a "CometMLClient" similar to your App client to abstract all of this
-    # logic and querying of the CometML servers away to keep it clean here
-
-    # raise NotImplementedError("TODO: implement this endpoint")
-
-    # response = MODEL_DIR
-
-    app.logger.info(response)
+                #saving failed
+                else:
+                    exception = f"{model_name} failed saving to local, the loaded model remains."
+                    return jsonify(exception),400
+            #download fail
+            except Exception:
+                exception = f"{model_name} failed downloading, the loaded model remains."
+                return jsonify(exception),400
+    
     return jsonify(response)  # response must be json serializable!
 
 
@@ -157,15 +183,19 @@ def predict():
     json = request.get_json()
     app.logger.info(json)
 
-    X = json["X"]
-    # # TODO:
-    # raise NotImplementedError("TODO: implement this enpdoint")
+    # Make sure the features are filtered before sending request
+    X_filtered = pd.read_json(json)
     
-    # response = None
+    try:
+        probas = LOADED_MODEL.predict_proba(X_filtered)
+        probas_df =pd.DataFrame(probas[:,1],columns=['chance of goal'])
 
-    # app.logger.info(response)
-    # return jsonify(response)  # response must be json serializable!
-    return jsonify(json)
+        app.logger.info("Predicted successfully.")
+        return jsonify(probas_df.to_json())
+    
+    except Exception:
+        return jsonify("Prediction failed."),400
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.environ.get("SERVING_PORT", 8080))
+# Use gunicorn/waitress
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=os.environ.get("SERVING_PORT", 8080))
