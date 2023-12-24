9f59fd131d8cadadf3659aedadf81458395d4578
Hamidreza Fereidouni <hfereidouni@users.noreply.github.com>
2023-12-23 22:09:37 -0500

    Add files via upload

---
+++ Milestone3/Code2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# In termimal
+# e.g bash run.sh <image_name> <COMET_API_KEY>
+# v5q8O8LftZtvOcoXlVM8Ku8fH
+docker run -p 5000:8080 -e COMET_API_KEY="$2" $1
+# docker run -p 8080:8080 -e COMET_API_KEY="${COMET_API_KEY}" $1
