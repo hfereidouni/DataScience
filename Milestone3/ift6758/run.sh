f23f2ba32e47f2f289a8d3d1ecfac2049eed325a
Hamidreza Fereidouni <hfereidouni@users.noreply.github.com>
2023-12-23 23:48:22 -0500

    Update run.sh

--- Milestone3/ift6758/run.sh
+++ Milestone3/ift6758/run.sh
@@ -3,5 +3,5 @@
 # In termimal
 # e.g bash run.sh <image_name> <COMET_API_KEY>
 # v5q8O8LftZtvOcoXlVM8Ku8fH
-docker run -p 5000:8080 -e COMET_API_KEY="$2" $1
-# docker run -p 8080:8080 -e COMET_API_KEY="${COMET_API_KEY}" $1
+docker run -p 5000:5000 -e COMET_API_KEY="$2" $1
+# docker run -p 5000:5000 -e COMET_API_KEY="${COMET_API_KEY}" $1
